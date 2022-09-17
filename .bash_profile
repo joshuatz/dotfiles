b59eccd2df306352f3cc204b69be544df9d72096
Joshua Tzucker <joshua.tz@gmail.com>
2022-09-16 23:00:46 -0700

    Add some initial functions, aliases, and configs
    - Some ZSH specific config stuff
    - General functions, aliases, etc.
    - Some bootstrap code (WIP)
    - Placeholder Taskfile
    - Git & tmux config files

---
+++ .bash_profile
@@ -0,0 +1,1 @@
+# Stub - see .zshrc
---
+++ copy.sh
@@ -0,0 +1,6 @@
+cp .zshrc ~/.zshrc
+cp .functions ~/.functions
+cp .aliases ~/.aliases
+cp global.gitignore ~/global.gitignore
+cp .gitconfig ~/.gitconfig
+cp Taskfile.global.yml ~/Taskfile.global.yml
